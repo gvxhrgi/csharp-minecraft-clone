7609977a2832dbc97b067a86cfad5b73f3151589
gvxhrgi <134945335+gvxhrgi@users.noreply.github.com>
2025-06-19 13:41:25 +0000

    Fixed Resizing

--- minecraft_clone/Game.cs
+++ minecraft_clone/Game.cs
@@ -14,13 +14,45 @@ namespace OpenTK_Minecraft_Clone
     internal class Game : GameWindow
     {
         //CONSTANTS
-        private static int SCREEN_WIDTH;
-        private static int SCREEN_HEIGHT;
+        int width, height;
         public Game(int width, int height) : base(GameWindowSettings.Default, NativeWindowSettings.Default)
         {
-            this.CenterWindow(new Vector2i(width, height));
-            SCREEN_WIDTH = width;
-            SCREEN_HEIGHT = height;
+            this.width = width;
+            this.height = height;
+            CenterWindow(new Vector2i(width, height));
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+            GL.Viewport(0, 0, e.Width, e.Height);
+            this.width = e.Width;
+            this.height = e.Height;
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+        }
+
+        protected override void OnUnload()
+        {
+            base.OnUnload();
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            GL.ClearColor(0.6f, 0.3f, 1f, 1f);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            Context.SwapBuffers();
+            
+            base.OnRenderFrame(args);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs args)
+        {
+            base.OnUpdateFrame(args);
         }
     }
 }
